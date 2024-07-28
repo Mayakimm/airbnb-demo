d0d598cdd4e7dff7ed63fe898c5f2cacef77efb1
Maya Kim <mayagy.kim@gmail.com>
2024-07-28 21:44:30 +0200

    built basic structure of create reviews and display reviews

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   def show
     @bookings = Booking.where(user: current_user)
+    @upcoming_bookings = @bookings.where("start_date > ?", Date.today)
+    @past_bookings = @bookings.where("start_date < ?", Date.today)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   resources :users, only: %i[show]
   resources :flats, only: %i[show new create edit update destroy]
 
-  resources :bookings, only: %i[show create destroy]
+  resources :bookings, only: %i[show create destroy] do
+    resources :reviews, only: %i[new create destroy]
+  end
 
-  resources :reviews, only: %i[new create destroy]
 
 end
