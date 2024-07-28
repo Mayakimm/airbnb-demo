88dd0c209277809bfb4303206d57d805f444a640
Maya Kim <mayagy.kim@gmail.com>
2024-07-28 14:03:13 +0200

    generated user, flat, booking, and review models

---
+++ db/migrate/20240728120009_create_bookings.rb
@@ -0,0 +1,13 @@
+class CreateBookings < ActiveRecord::Migration[7.1]
+  def change
+    create_table :bookings do |t|
+      t.date :start_date
+      t.date :end_end
+      t.boolean :confirm
+      t.references :user, null: false, foreign_key: true
+      t.references :flat, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
