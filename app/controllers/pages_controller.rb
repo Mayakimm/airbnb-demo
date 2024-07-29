5bfe84f316a94e292f934ae0093ff567df51b02c
Maya Kim <mayagy.kim@gmail.com>
2024-07-29 17:00:53 +0200

    search bar implemented

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
   def home
     @flats = Flat.all
+    if params[:query].present?
+      sql_subquery = <<~SQL
+        flats.name @@ :query
+        OR flats.address @@ :query
+      SQL
+      @flats = @flats.where(sql_subquery, query: "%#{params[:query]}%")
+    end
   end
 end
